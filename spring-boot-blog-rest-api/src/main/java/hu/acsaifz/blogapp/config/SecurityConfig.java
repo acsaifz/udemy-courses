1fcd2172de5627cde6c626eee7b0db7e9b5c3622
acsaifz <angeluszmail@gmail.com>
2023-01-24 11:23:12 +0100

    implement simple dao authentication management

--- spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/config/SecurityConfig.java
+++ spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/config/SecurityConfig.java
@@ -1,27 +1,34 @@
 package hu.acsaifz.blogapp.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+    private final UserDetailsService userService;
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -36,23 +43,5 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User
-                .builder()
-                .username("teszt")
-                .password(passwordEncoder().encode("teszt"))
-                .roles("USER").build();
-
-        UserDetails admin = User
-                .builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
---
+++ spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package hu.acsaifz.blogapp.repository;
+
+import hu.acsaifz.blogapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
