3f372f9c073dcfce49d228c74b346aa49886d64e
acsaifz <angeluszmail@gmail.com>
2023-02-03 15:14:37 +0100

    fix

--- spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/config/JwtAuthenticationFilter.java
+++ spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/config/JwtAuthenticationFilter.java
@@ -7,18 +7,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
-@Component
+@Configuration
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenService jwtTokenService;
     private final UserService userService;
--- spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/model/dto/auth/JwtTokenDto.java
+++ spring-boot-blog-rest-api/src/main/java/hu/acsaifz/blogapp/model/dto/auth/JwtTokenDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class JwtTokenDto {
     private String accessToken;
-    private String tokenType = "Bearer";
+    private final String tokenType = "Bearer";
 
     public JwtTokenDto(String accessToken) {
         this.accessToken = accessToken;
